1e8a3f4d30a6a1ef5fb3b7d7495162ebfafb7479
Pedro Rothen <p.rothen@outlook.cl>
2017-02-08 08:34:02 -0300

    Initial Commit

---
+++ ObjectiveCForSwiftDevelopersCourse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCForSwiftDevelopersCourse
+//
+//  Created by Pedro on 2/8/17.
+//  Copyright © 2017 Rothen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
